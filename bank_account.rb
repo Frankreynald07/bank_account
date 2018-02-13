c02915c6903a5393de915790a6ab7c53d0f0e05d
Frank Ruiz <sanreynaldo@gmail.com>
2018-02-12 23:20:45 -0500

    Bank account OOP practice

--- bank_account.rb
+++ bank_account.rb
@@ -1,3 +1,34 @@
 class BankAccount
-  
+  @@minimum_balance = 100
+  attr_accessor :balance
+  def initialize(opening_balance, client_name)
+    @balance = opening_balance
+    if @balance < 200
+        raise ArgumentError.new "minimum balance not met"
+    end
+  end
+  def self.minimum_balance=(minimum_balance)
+    @@minimum_balance = minimum_balance
+     return @@minimum_balance
+  end
+  def self.minimum_balance
+    @@minimum_balance
+  end
+  def deposit(amount)
+    @balance += amount
+  end
+  def withdraw(w_amount)
+    if w_amount > @balance
+      begin
+        raise StandardError.new "insufficient funds"
+      rescue
+        return nil
+      end
+    end
+    @balance -= w_amount
+  end
+def transfer(amount, bank_account)
+  @balance -= amount
+  bank_account.deposit(amount)
+  end
 end
--- spec/bank_account_spec.rb
+++ spec/bank_account_spec.rb
@@ -1,25 +1,69 @@
 require './bank_account'
-
-
-describe BankAccount do
-
-  context "has a balance"
-  it "is created with an opening balance and the name of the client"
-  it "can report it's balance"
-
-context "making a deposit"
-  it "balance is increased"
-
-context "making a withdrawal"
-  it "balance is decreased"
-
-context "transfering funds"
-  it "account balance is decreased"
-  it "other account balance is increased"
-
-context "minimum balance"
-  it "raises an error if opening balance is too low"
-  it "does NOT raise an error if opening balance is over minimum balance"
-  it "allows the bank owner to change the minimum balance"
-
+    describe BankAccount do
+    context "has a balance" do
+    let(:account) do
+    BankAccount.new(500, "Sarah")
+    end
+    it "is created with an opening balance and the name of the client" do
+    expect(account).to be_a(BankAccount)
+    end
+    it "can report it's balance" do
+    expect(account.balance).to eq(500)
+    end
+    end
+    context "making a deposit" do
+    let(:account) do
+    account = BankAccount.new(500, "Sarah")
+    account.deposit(500)
+    account
+    end
+    it "balance is increased" do
+    expect(account.balance).to eq(1000)
+    end
+    end
+    context "making a withdrawal" do
+    let(:account) do
+     account = BankAccount.new(500, "Sarah")
+     account.withdraw(200)
+     account
+    end
+    it "balance is decreased" do
+     expect(account.balance).to eq(300)
+    end
+    it "excessive withdraw amount" do
+      expect(account.withdraw(9999)).to eq(nil)
+    end
+    context "transfering funds" do
+     let(:account) do
+       account = BankAccount.new(500, "Sarah")
+     end
+     let(:other_account) do
+       other_account = BankAccount.new(1000, "Todd")
+     end
+     before :each do
+       account.transfer(200, other_account)
+     end
+     it "account balance is decreased" do
+       expect(account.balance).to eq(300)
+     end
+     it "other account balance is increased" do
+       expect(other_account.balance).to eq(1200)
+     end
+    end
+    end
+    it "throws an error if minimum opening balance of 200 is not met" do
+    expect { BankAccount.new(100, "Sarah") }.to raise_error(ArgumentError)
+    end
+    context "minimum balance" do
+    it "raises an error if opening balance is too low" do
+    expect{ BankAccount.new(199, "Terry") }.to raise_error(ArgumentError)
+    end
+    it "does NOT raise an error if opening balance is over minimum balance" do
+    expect{ BankAccount.new(201, "Terry") }.not_to raise_error
+    end
+    it "allows the bank owner to change the minimum balance" do
+    BankAccount.minimum_balance = 100
+    expect(BankAccount.minimum_balance).to eq(100)
+    end
+    end
 end
